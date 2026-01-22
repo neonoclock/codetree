cecf09a87a887cba35bf1c563400fa84870ff923
Hyewon Ahn <neonoclock213@naver.com>
2026-01-22 11:33:06 +0900

    Title: 세 실수의 반올림, Time: 64ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260122/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let input = Number(fs.readFileSync(0).toString().trim('/n'));
+
+let a = input[0];
+let b = input[1];
+let c = input[2];
+
+console.log(`${a.toFixed(3)}`);
+console.log(`${a.toFixed(3)}`);
