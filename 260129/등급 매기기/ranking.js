7e799f9ad9652b067da3a077d9b81eb750505962
Hyewon Ahn <neonoclock213@naver.com>
2026-01-29 11:48:59 +0900

    Title: 등급 매기기, Time: 72ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260129/등급 매기기/ranking.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let N = fs.readFileSync(0).toString().trim();
+
+if (N>=90) {
+    console.log("A")
+} else if (N<90 && N>=80) {
+    console.lod("B")
+} else if (N<80 && N>=70) {
+    console.lod("C")
+} else if (N<70 && N>=60) {
+    console.lod("D")
+} else {
+    console.log("F")
+}
