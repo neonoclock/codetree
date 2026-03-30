56de7450b717b598694fe3d23a757c0fd90034f7
Hyewon Ahn <neonoclock213@naver.com>
2026-03-30 10:41:38 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/비교 연산/comparison-operator.py
@@ -0,0 +1,34 @@
+inp = input()
+arr = inp.split()
+A = int(arr[0])
+B = int(arr[1])
+
+if A>=B:
+    print('1')
+else:
+    print('0')
+
+if A>B:
+    print('1')
+else:
+    print('0')
+
+if A<=B:
+    print('1')
+else:
+    print('0')
+
+if A<B:
+    print('1')
+else:
+    print('0')
+
+if A==B:
+    print('1')
+else:
+    print('0')
+
+if A!=B:
+    print('1')
+else:
+    print('0')
