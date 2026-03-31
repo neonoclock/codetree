2962bdf0ae10d0311971416b9541d88d5cb436a8
Hyewon Ahn <neonoclock213@naver.com>
2026-03-31 11:02:41 +0900

    Title: 배수의 개수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260331/배수의 개수/number-of-multipliers.py
+++ 260331/배수의 개수/number-of-multipliers.py
@@ -1,8 +1,12 @@
-A = int(input())
-A = int(input())
-A = int(input())
-A = int(input())
-A = int(input())
-A = int(input())
-A = int(input())
-A = int(input())
+arr = list(map(int, input().split()))
+
+cnt3 = 0
+cnt5 = 0
+
+for i in arr:
+    if i % 3 ==0:
+        cnt3 += 1
+    elif i % 5 ==0:
+        cnt5 += 1
+
+print(cnt3, cnt5)
