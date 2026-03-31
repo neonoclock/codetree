374e6ffbb5b3db05e697e507a00c322e61b92e87
Hyewon Ahn <neonoclock213@naver.com>
2026-03-31 11:12:41 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260331/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -0,0 +1,11 @@
+arr = ["apple", "banana", "grape", "blueberry", "orange"]
+ch = input()
+
+cnt = 0
+
+for word in arr:
+    if word[2] == ch or word[5] ==ch:
+        print(word)
+        cnt += 1
+
+print(cnt)
