8cffa554050f1531051aebd800253f91b6941a5b
Hyewon Ahn <neonoclock213@naver.com>
2026-04-02 12:43:19 +0900

    Title: 상하로 반복하며 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+x=1
+
+for i in range(1, N+1):
+    for j in range(N):
+        if j%2==0:
+            print(i, end='')
+        else:
+            print((N-i+1), end='')
+    print()
